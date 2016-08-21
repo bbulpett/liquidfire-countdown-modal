f05c347fb060cb9f643d42e7ba21be3b68a43281
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-08-20 23:36:17 -0400

    Initial counter property setup

--- app/components/countdown-modal.js
+++ app/components/countdown-modal.js
@@ -1,10 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  counter: 0,
+
   actions: {
     dismiss() {
       this.sendAction('dismiss');
     },
-    
+
+    startCounter() {
+      Ember.run.later(this, function() {
+      var counter = this.get('counter');
+      this.set('counter', counter++);
+      }, 1000);
+    },
+
+    resetCounter() {
+      this.set('counter', 0)
+    }
   }
 });
